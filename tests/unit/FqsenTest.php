57d2d4832d36fb1a99728bb892fbe019431c71e9
Jaapio <jaap@ijaap.nl>
2015-06-02 11:08:13 +0200

    initialize the project

---
+++ tests/unit/FqsenTest.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * phpDocumentor
+ *
+ * PHP Version 5.5
+ *
+ * @copyright 2010-2015 Mike van Riel / Naenius (http://www.naenius.com)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://phpdoc.org
+ */
+
+namespace phpDocumentor\Reflection;
+
+/**
+ * Class FqsenTest
+ * @coversDefaultClass phpDocumentor\Reflection\Fqsen
+ */
+class FqsenTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @param string $fqsen
+     * @covers ::__construct
+     * @dataProvider validFqsenProvider
+     */
+    public function testValidFormats($fqsen)
+    {
+        new Fqsen($fqsen);
+    }
+
+    /**
+     * Data provider for ValidFormats tests. Contains a complete list from psr-5 draft.
+     *
+     * @return array
+     */
+    public function validFqsenProvider()
+    {
+        return [
+            ['\My\Space'],
+            ['\My\Space\myFunction()'],
+            ['\My\Space\MY_CONSTANT'],
+            ['\My\Space\MyClass'],
+            ['\My\Space\MyInterface'],
+            ['\My\Space\MyTrait'],
+            ['\My\Space\MyClass::myMethod()'],
+            ['\My\Space\MyClass::$my_property'],
+            ['\My\Space\MyClass::MY_CONSTANT'],
+        ];
+    }
+
+    /**
+     * @param string $fqsen
+     * @covers ::__construct
+     * @dataProvider invalidFqsenProvider
+     * @expectedException \InvalidArgumentException
+     */
+    public function testInValidFormats($fqsen)
+    {
+        new Fqsen($fqsen);
+    }
+
+    /**
+     * Data provider for invalidFormats tests. Contains a complete list from psr-5 draft.
+     *
+     * @return array
+     */
+    public function invalidFqsenProvider()
+    {
+        return [
+            ['\My\*'],
+            ['\My\Space\.()'],
+            ['My\Space'],
+        ];
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::__toString
+     */
+    public function testToString()
+    {
+        $className = new Fqsen('\\phpDocumentor\\Application');
+
+        $this->assertEquals('\\phpDocumentor\\Application', (string)$className);
+    }
+}
